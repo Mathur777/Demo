b5fd1363b8e36f40d77a10ea7f5e6f8a3e16f02b
sumit <sumitmathurl777@gmail.com>
2020-04-12 10:32:15 +0530

    my first commite

---
+++ flash/src/flash/First.java
@@ -0,0 +1,24 @@
+package flash;
+
+
+class First{
+
+static int myplus(int x, int y){
+	return x+y;
+
+}
+static double myplus(double x, double y){
+	return x+y;
+
+}
+
+	public static void main(String[] args) {
+	 int num1= myplus(67,56);
+	 double num2= myplus(2.4,4.5);
+	 System.out.println(num1);
+	 System.out.println(num2);
+
+
+
+	}
+}
