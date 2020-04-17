32a2bf6dd54b6401ac76bc45d91ceb47b8bdaf57
sumit <sumitmathurl777@gmail.com>
2020-04-17 19:47:37 +0530

    code updated with argument constructor

--- flash/src/flash/MyConstructor.java
+++ flash/src/flash/MyConstructor.java
@@ -10,11 +10,17 @@ class cube{
 		return (hieght*bredth*lenghth);
 		
 	}
-	cube(){    //constructor...
+	cube(){    // default constructor...
 		hieght=23;
 		bredth=56;
 		lenghth=34;
 		
+	}
+	cube(int h, int b, int l){ //argument constructor..
+		
+		hieght=h;
+		bredth=b;
+		lenghth=l;
 	}
 }
 
@@ -27,8 +33,9 @@ public class MyConstructor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-cube sc=new cube();  
-System.out.println(sc.getvalue());
-
+cube sc=new cube();   
+System.out.println( "I m default "+sc.getvalue());
+ cube sc2=new cube(9,7 ,7);
+ System.out.println( " i m arument "+sc2.getvalue());
 }
 }
