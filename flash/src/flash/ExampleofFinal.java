6dd46c4c49edd8740efc3f4bb0a89894ac660844
sumit <sumitmathurl777@gmail.com>
2020-04-21 10:14:42 +0530

    final keyword example

---
+++ flash/src/flash/ExampleofFinal.java
@@ -0,0 +1,28 @@
+package flash;
+
+
+class Aim  {
+	
+	public final int  num;
+	
+	Aim(){
+		
+		num=90;
+	}
+}
+
+
+
+
+
+
+public class ExampleofFinal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Aim sc=new Aim();
+	
+	}
+
+}
